2262df12f814b2c8539915cf376b8f379bc89e96
Viktoria Kaltovich <38467721+Victaria@users.noreply.github.com>
2019-10-28 16:14:06 +0300

    SQL Insert tested

--- src/sample/SqlConnection/ConnectDB.java
+++ src/sample/SqlConnection/ConnectDB.java
@@ -10,19 +10,11 @@ public class ConnectDB {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://206.189.102.66:3306/db";
             con = DriverManager.getConnection(url, "WT", "sk7SszGA2uSL3ZkM");
-           // String query = "SELECT id, name FROM Products";
+           
             String query = "INSERT INTO Products (name, price, amount) VALUES('chili', 225, 5)";
             Statement stmt = con.createStatement();
             stmt.executeUpdate(query);
-            
-           // ResultSet rs = stmt.executeQuery(query);
-           /* while (rs.next()) {
-                int count = rs.getInt(1);
-                System.out.println("Total number of records in the table : " + count);
-                System.out.println(rs.getString(2));
-            }*/
 
-            //rs.close();
             stmt.close();
             con.close();
         } catch (SQLException sqlEx) {
